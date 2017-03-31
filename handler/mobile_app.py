5f32d22c1e5fc6a1c9d15572dd054b56fb09ce12
liuxiaoming <liuxiaoming@ddc6bd4c-53ed-4c45-b049-ad24887e61f7>
2017-03-31 01:34:22 +0000

    修复注册验证码检查的bug

--- handler/mobile_app.py
+++ handler/mobile_app.py
@@ -122,7 +122,7 @@ class MobileCheckVCodeAppHandler(MobileBaseHandler):
         mobile = self.get_body_argument('mobile', None)
         vcode = self.get_body_argument('vcode', None)
         flag = self.get_body_argument("flag", None)
-        if VCode.check_vcode(mobile, vcode, flag):
+        if VCode.check_vcode(None, mobile, vcode, flag):
             result['flag'] = 1
         else:
             result['msg'] = "请输入正确的手机号或验证码"
