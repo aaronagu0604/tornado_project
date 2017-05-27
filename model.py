9bd837f3ea3215ad9c9a4fe89701482787cf1235
liuxiaoming <liuxiaoming@ddc6bd4c-53ed-4c45-b049-ad24887e61f7>
2017-05-27 11:27:36 +0000

    分类h5页面完善

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import time
 from peewee import *
 import hashlib
 #from bootloader import db
-from bootloader import db_move as db
+from bootloader import db_move3 as db
 from lib.util import vmobile
 import re
 import setting
