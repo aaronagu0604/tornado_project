5feae3f2d162c251a7ec98ae5e455932278380bf
liuxiaoming <liuxiaoming@ddc6bd4c-53ed-4c45-b049-ad24887e61f7>
2017-06-04 11:49:20 +0000

    分类h5页面完善

--- insurance_policy.py
+++ insurance_policy.py
@@ -6140,7 +6140,7 @@ def update_store_policy():
         insurace_area = InsuranceArea.select().where(
             InsuranceArea.area_code == store.area_code[:8] & InsuranceArea.active == 1)
         for item in insurace_area:
-            print store.name, item.insurance.name
+            print store.name, item.insurance.name, store.area_code
             SSILubePolicy.create(store=store,
                                      insurance=item.insurance,
                                      cash=item.cash_policy,
@@ -6150,7 +6150,7 @@ def update_store_policy():
         insurace_area = InsuranceArea.select().where(
             InsuranceArea.area_code == store.area_code[:4] & InsuranceArea.active == 1)
         for item in insurace_area:
-            print store.name, item.insurance.name
+            print store.name, item.insurance.name, store.area_code
             SSILubePolicy.create(store=store,
                                      insurance=item.insurance,
                                      cash=item.cash_policy,
